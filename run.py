302b6ba4195a3eaf59a37ffc60ab579d879992e2
Sebrah <seb.ammon@gmail.com>
2019-05-18 17:36:12 +0200

    Proper GPIO cleanup

--- run.py
+++ run.py
@@ -2,8 +2,6 @@ from securit import app
 from securit.sensors import GPIO
 
 if __name__ == "__main__":
-    try:
-        app.run(use_reloader=False, host='0.0.0.0')
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-        print('GPIO pins were cleaned!')
+    app.run(use_reloader=False, host='0.0.0.0')
+    GPIO.cleanup()
+    print('GPIO pins were cleaned!')
