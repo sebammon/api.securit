4b48c6839ed6e7a60741669e9eea235960ed3b7e
Sebastian Ammon <seb.ammon@gmail.com>
2019-08-18 12:19:59 +0200

    Change to swagger UI

--- securit/app.py
+++ securit/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
 from securit import config
 from securit.system import Alarm, Statuses
+from flask_restplus import Resource, Api
 
 app = Flask(__name__)
-config = config.DevelopmentConfig if app.config.get('ENV') == 'development' else config.ProductionConfig
+api = Api(app)
+config = config.DevelopmentConfig if app.config.get(
+    'ENV') == 'development' else config.ProductionConfig
 app.config.from_object(config)
 
 alarm = Alarm()
@@ -13,23 +16,26 @@ from securit.sensors import initialise_sensors
 
 initialise_sensors()
 
-@app.route('/')
-def server_running():
-    return 'Server is running'
 
-@app.route('/alarm/status', methods=['GET'])
-def get_alarm_status():
-    status = alarm.get_status()
-    return jsonify({'status': status})
+@api.route('/server')
+class Server(Resource):
+    def get(self):
+        return 'Server is running'
 
-@app.route('/alarm/status', methods=['POST'])
-def set_alarm_status():
-    status = request.form.get('status')
 
-    if status is not None:
-        new_status = status.upper()
-        if new_status in [status.name for status in Statuses]:
-            alarm.set_status(status)
-            return jsonify({'status': new_status})
-            
-    return jsonify({'message': 'Please provide a valid status.'})
+@api.route('/alarm/status')
+class AlarmStatus(Resource):
+    def get(self):
+        status = alarm.get_status()
+        return jsonify({'status': status})
+
+    def post(self):
+        status = request.form.get('status')
+
+        if status is not None:
+            new_status = status.upper()
+            if new_status in [status.name for status in Statuses]:
+                alarm.set_status(status)
+                return jsonify({'status': new_status})
+
+        return jsonify({'message': 'Please provide a valid status.'})
